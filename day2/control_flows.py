e43cf3f42c30387690355bbab8b8ae714d591f18
Muriithi Gakuru <76877744+morehwachege@users.noreply.github.com>
2022-11-22 20:19:32 +0000

    control flows

--- day2/control_flows.py
+++ day2/control_flows.py
@@ -1,1 +1,43 @@
 # explore control flows
+# if statements
+# for loops
+# while loops
+
+name = "James"
+number = 20
+
+# if statements
+# if name == "James":
+#     print("James is awesome")
+# else:
+#     print("Something else")
+
+# # for loops
+# for x in name:
+#     print(x)
+
+# # while loops
+# while True:
+#     print("Never ending")
+
+
+# print(range(10))
+# [0, 1,2,3,4,5,6,7,8,9]
+
+# for x in range(10):
+#     print(x)
+count = 0
+
+# for x in range(10):
+    
+
+while True:
+    # break
+    count += 1
+    print(count)
+    if(count == 5):
+        break
+    
+    # pass
+
+
---
+++ day2/looping_techniques.py
@@ -0,0 +1,35 @@
+# lists
+# dictionaries
+# tuples
+# sets
+
+my_list = ['James', "Kenny", 1202, "Indian"]
+my_tuple = ('James', "Kenny", 1202, "Indian")
+
+# my_list[0]
+# for item in my_list:
+#     print(item)
+
+my_dictionary = {
+    "name": "Jen",
+    "age": 34,
+    "location": "Malindi"
+}
+# print(my_dictionary["name"])
+
+# for key, value in my_dictionary.items():
+#     print(key, ":", value)
+
+# for key in my_dictionary.keys():
+#     print(key)
+
+# for value in my_dictionary.values():
+#     print(value)
+
+name = ["Indian", "Kenny", 'James', "Kenny", "Maalia", "Indian", "Kenny"]
+name.count("Kenny")
+updated_name = set(name)
+
+# sys
+# os
+# 
