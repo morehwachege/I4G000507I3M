15afd5525087676d99ae40a3ea416aa2d7f009be
Muriithi Gakuru <antony123muriithi@gmail.com>
2022-11-27 20:13:23 +0300

    remove environ

--- day5/environ.py
+++ day5/environ.py
@@ -1,7 +1,7 @@
 import os
 
-env = os.environ['VERY_SECRET_KEY']
-print(env)
+password = os.environ['VERY_SECRET_KEY']
+print(password)
 # env from terminal with
 # set ENV_VAR="variable item"
 # printenv to print env variable
